06aa0087d622abc52030e395bd9668961caa79b4
Wayne <bwh.buaa@gmail.com>
2020-12-01 11:20:18 -0500

    first version: CONVERT AST -> CODE

--- ast2js.py
+++ ast2js.py
@@ -13,8 +13,86 @@ def convertAST(node):
 
         seq_dic['type'] = "Program"
         seq_dic['sourceType'] = "script"
+        # seq_dic['sourceType'] = "module"
         seq_dic['body'] = []
         seq_dic['seqN'] = node['id']
+
+    elif node['type'] == "ArrayPattern":
+        seq_dic['type'] = "ArrayPattern"
+        seq_dic['elements'] = []
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == "RestElement":
+        seq_dic['type'] = "RestElement"
+        seq_dic['argument'] = {}
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == 'AssignmentPattern':
+        seq_dic['type'] = "AssignmentPattern"
+        seq_dic['left'] = {}
+        seq_dic['right'] = {}
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == 'ObjectPattern':
+        seq_dic['type'] = "ObjectPattern"
+        seq_dic['properties'] = []
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == 'ThisExpression':
+        seq_dic['type'] = "ThisExpression"
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == "Identifier":
+        seq_dic['type'] = "Identifier"
+        seq_dic['name'] = node['name']
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == "Literal":
+        seq_dic['type'] = "Literal"
+        seq_dic['value'] = node['value']
+        seq_dic['raw'] = node['raw']
+        seq_dic['seqN'] = node['id']
+        if 'regex' in node.keys():
+            seq_dic['regex'] = node['regex']
+
+    elif node['type'] == 'ArrayExpression':
+        seq_dic['type'] = 'ArrayExpression'
+        seq_dic['elements'] = []
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == 'ObjectExpression':
+        seq_dic['type'] = "ObjectExpression"
+        seq_dic['properties'] = []
+        seq_dic['seqN'] = node['id']
+
+    elif node['type'] == 'Property':
+        seq_dic['type'] = 'Property'
+        seq_dic['key'] = {}
+        seq_dic['computed'] = node['computed']
+        seq_dic['value'] = {}
+        seq_dic['kind'] = node['kind']
+        seq_dic['method'] = node['method']
+        seq_dic['shorthand'] = node['shorthand']
+
+    elif node['type'] == 'FunctionExpression':
+        seq_dic['type'] = "FunctionExpression"
+        seq_dic['id'] = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     elif node['type'] == "VariableDeclaration":
         seq_dic['type'] = "VariableDeclaration"
@@ -28,16 +106,7 @@ def convertAST(node):
         seq_dic['init'] = {}
         seq_dic['seqN'] = node['id']
 
-    elif node['type'] == "Identifier":
-        seq_dic['type'] = "Identifier"
-        seq_dic['name'] = node['value']
-        seq_dic['seqN'] = node['id']
 
-    elif node['type'] == "Literal":
-        seq_dic['type'] = "Literal"
-        seq_dic['value'] = node['value']
-        seq_dic['raw'] = node['value']
-        seq_dic['seqN'] = node['id']
 
     return seq_dic
 
@@ -49,6 +118,29 @@ def mergeAST(dic1,dic2,idx):
             if idx == v:
                 if dic1['type'] == 'Program':
                     dic1['body'].append(dic2)
+                elif dic1['type'] == 'ArrayPattern':
+                    dic1['elements'].append(dic2)
+                elif dic1['type'] == 'RestElement':
+                    dic1['argument'] = dic2
+                elif dic1['type'] == 'AssignmentPattern':
+                    if dic2['type'] == 'Identifier' or dic2['type'] == 'BindingPattern':
+                        dic1['left'] = dic2
+                    if dic2['type'] == 'Expression':
+                        dic1['right'] = dic2
+                elif dic1['type'] == 'ObjectPattern':
+                    dic1['properties'].append(dic2)
+                elif dic1['type'] == 'ArrayExpression':
+                    dic1['elements'].append(dic2)
+                elif dic1['type'] == 'ObjectExpression':
+                    dic1['properties'].append(dic2)
+                elif dic1['type'] == 'Property':
+
+
+
+
+
+
+
                 elif dic1['type'] == 'VariableDeclaration':
                     dic1['declarations'].append(dic2)
                 elif dic1['type'] == 'VariableDeclarator':
